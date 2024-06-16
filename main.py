6f3236fdd9360dc99a81fe222251203579e33e97
zhujch1 <eddyzhu123@163.com>
2024-06-17 03:19:46 +0800

    fixed bugs

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ import topopt_2D
 import topopt_3D
 
 def demo_3D_1():
-    solver = topopt_3D.FEM_TopOpt_Solver_3D(nx=40, ny=20, nz=10, volfrac=0.3, penal=3.0, 
+    nx, ny, nz = 40, 20, 10
+    solver = topopt_3D.FEM_TopOpt_Solver_3D(nx=nx, ny=ny, nz=nz, volfrac=0.3, penal=3.0, 
                                          rho_min=0.001, filter_radius=1.5, E=1.0, nu=0.3)
     
     # Load in the center of the top face
     load = np.array([
-        solver._index(20, 10, 10) + 2
+        solver._index(nx // 2, ny // 2, nz) + 2
     ])
 
     # Support on the four bottom corners
     bc = np.array([
-        solver._index(0, 0, 0),  solver._index(0, 30, 0),
-        solver._index(60, 0, 0), solver._index(60, 30, 1)
+        solver._index(0, 0, 0),  solver._index(0, ny, 0),
+        solver._index(nx, 0, 0), solver._index(nx, ny, 0)
     ])
     bc = np.concatenate([bc, bc + 1, bc + 2])
 
@@ -50,8 +51,8 @@ def demo_2D_2():
 
 if __name__ == '__main__':
     # 2D demo
-    demo_2D_1()
-    demo_2D_2()
+    # demo_2D_1()
+    # demo_2D_2()
 
     # 3D demo
     demo_3D_1()
